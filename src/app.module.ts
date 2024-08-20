f880418bb274a03a69578814833b58291137a2d8
Ivand14 <carpivan14@hotmail.com>
2024-08-20 18:16:51 -0300

    utilizacion de docker, creacion de mongodb.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'),
+      }),
+    PokemonModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon') 
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,41 @@
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { Injectable } from '@nestjs/common';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+import {Model} from 'mongoose'
+
+@Injectable()
+export class PokemonService {
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly PokemonModel: Model<Pokemon>
+  ){}
+  
+  async create(createPokemonDto: CreatePokemonDto) {
+
+    createPokemonDto.name = createPokemonDto.name.toLowerCase()
+
+    const newPokemon = await this.PokemonModel.create( createPokemonDto )
+
+    return newPokemon
+
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pokemon`;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${id} pokemon`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pokemon`;
+  }
+}
